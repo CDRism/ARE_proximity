9bb229c28227e8e4c6bee4720cc4ec9d29f81439
Chris Robinson <49954964+CDRism@users.noreply.github.com>
2024-06-25 10:26:34 -0400

    ARE_gene_intersection_typeierror.R

--- ARE_gene_intersection_typeierror.R
+++ ARE_gene_intersection_typeierror.R
@@ -11,17 +11,15 @@ p = NULL
 
 pvals = foreach(i = 1:100, .combine = 'comb', .multicombine = TRUE,
                 .init = list(p)) %dopar%{
-                  a = sample(1:length(data$gene),373)
+                  a = sample(1:length(data$gene),373) #change 373 to relevant value (see ARE_gene_intersection_function.R)
                   
                   sub = data[a,]
                   
-                  randRE(data$gene, data$ARE, 373, 10000)
+                  randRE(data$gene, data$ARE, 373, 10000) #change 373 to relevant value 
                   p = length(which(nullDist$RE > mean(sub$ARE)))/10000
                 }
 parallel::stopCluster(cl = cluster)
 
 typeierrorrate = plyr::ldply(pvals[[1]])
-write.csv(typeierrorrate, "file_name.csv")
-
-data = read.csv("file_name.csv")
-length(which(data$V1 < 0.025 | data$V1 > 0.975))/10000 #0.0499! Yay!
+length(which(typeierrorrate$V1 < 0.025 | typeierrorrate$V1 > 0.975))/10000
+#want this to be less than 0.05
